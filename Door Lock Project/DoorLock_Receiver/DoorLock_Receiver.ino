c4732faf90ab22457792c8b55bbf26f82282c3f7
nmcgillicuddy <88528906+nmcgillicuddy@users.noreply.github.com>
2025-04-01 01:40:23 -0400

    Added servo integration to receiver

--- Door Lock Project/DoorLock_Receiver/DoorLock_Receiver.ino
+++ Door Lock Project/DoorLock_Receiver/DoorLock_Receiver.ino
@@ -5,14 +5,19 @@ send and turns on an LED
 
 #include <esp_now.h>
 #include <WiFi.h>
+#include <ESP32Servo.h>
 
 #define LED_PIN 3
+#define SERVO_PIN 8
+
 //mimicking struct from transmitter
 typedef struct struct_message {
   char userID[20];
 } struct_message;
 
 struct_message receivedData;
+
+Servo doorLock;
 
 void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   memcpy(&receivedData, incomingData, sizeof(receivedData));
@@ -26,13 +31,22 @@ void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   Serial.print("  | UserID: ");
   Serial.println(receivedData.userID);
 
+  //check for the lone valid user (temporary)
+  if (strcmp(receivedData.userID, "B3AC43DA") == 0) {
+    doorLock.write(90);
+  }
+
   digitalWrite(LED_PIN, HIGH);
   delay(1000);
   digitalWrite(LED_PIN, LOW);
+
+  delay(1000);
+  doorLock.write(0);
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); 
+  //setup ESP-NOW communication
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK) {
@@ -44,8 +58,13 @@ void setup() {
   //register receive callback
   esp_now_register_recv_cb(esp_now_recv_cb_t(onDataRecv));
 
+  //setup LED
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
+
+  //initialize servo
+  doorLock.setPeriodHertz(50);
+  doorLock.attach(SERVO_PIN, 500, 2400);
 }
 
 void loop() {
